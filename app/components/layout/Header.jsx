26629b19671ce4f457cd1195c2df5da6704d6cac
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2026-02-22 02:26:29 +0530

    add hamburger menu with collapsible mobile navigation

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 
 function Header() {
   return (
-    <header className="flex justify-between items-center max-mobile-nav:flex-row-reverse  h-20 bg-white">
+    <header className="flex justify-between items-center max-mobile-nav:flex-row-reverse  h-20 px-[5vw] bg-surface-0">
       <div className="w-fit text-nowrap">
         <a href="" className="border-none">
           Dr. Maya Reynolds, PsyD
--- app/components/layout/Nav.jsx
+++ app/components/layout/Nav.jsx
@@ -11,11 +11,23 @@ function Nav() {
 
   return (
     <nav className="w-full h-full flex items-center mobile-nav:justify-end">
-      <button className="max-mobile-nav:flex hidden" onClick={handleNav}>
-        Ham btn
+      <button
+        className="mobile-nav:hidden w-8 h-8 relative"
+        onClick={handleNav}
+      >
+        <span
+          className={`w-full h-px bg-text-primary/60 inline-block absolute top-2/6 left-1/2 -translate-1/2 ${!collapsed && " top-3/6 rotate-140"} transition-all ease-in duration-150 bg-text-primary`}
+        ></span>
+        <span
+          className={`w-full h-px bg-text-primary/60 inline-block absolute bottom-2/6 left-1/2 -translate-1/2 ${!collapsed && "bottom-3/6 -rotate-140"} transition-all  ease-in duration-100 bg-text-primary`}
+        ></span>
       </button>
+      <ul className={`flex flex-row max-mobile-nav:hidden`}>
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
       <ul
-        className={`flex max-mobile-nav:flex-col ${collapsed && "max-mobile-nav:hidden"}`}
+        className={`mobile-nav:hidden absolute left-0 top-20 w-full bg-surface-0 h-[calc(100vh-5rem)] flex flex-col items-center justify-center gap-8 text-fs-h3 transition-all duration-300 ease-in-out ${collapsed ? "scale-y-95 opacity-0 pointer-events-none" : "scale-y-100 opacity-100"} origin-bottom`}
       >
         <li>item 1</li>
         <li>item 2</li>
@@ -25,4 +37,5 @@ function Nav() {
 }
 
 export default Nav;
+
 
